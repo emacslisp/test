ce8edc5117482ad87ed0a9ea4df549f6d875a73d
emacslisp <java.gdb@gmail.com>
2019-12-10 13:48:06 +1100

    # - add syntax testing for C# 8

---
+++ cs/cs2019/cs2019/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs2019
+{
+    class Program
+    {
+        public struct Point
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+            public double Distance => Math.Sqrt(X * X + Y * Y);
+
+            public override string ToString() =>
+                $"({X}, {Y}) is {Distance} from the origin";
+        }
+
+        static void Main(string[] args)
+        {
+            Point p = new Point();
+            p.X = 1.0;
+            p.Y = 1.0;
+            Console.WriteLine(p);
+        }
+    }
+}
